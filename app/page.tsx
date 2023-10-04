bfddd31f979735668328887ffb3851dd69cbf7de
taiarima <tai.arima@colorado.edu>
2023-10-04 16:38:39 +0300

    Changed img to Image.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Posts from "./components/Posts";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="bg-slate-800 text-sky-50 min-h-screen">
-      <img
+      <Image
         className="mx-auto p-8 h-96"
         src="/tai-blog-silly-pic.png"
         alt="Tai Himself"
-      ></img>
+      ></Image>
       <h1 className="text-6xl text-center font-title font-bold mb-10">
         Making programming make sense
       </h1>
